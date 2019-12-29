ccd74973436d8937d9601eae08203fa5abcfaf2f
William Hendrick <william.hendrick@ipaper.com>
2019-12-29 17:43:52 -0600

    Adjustments

--- handler.js
+++ handler.js
@@ -1,23 +1,23 @@
 "use strict"
 
-const Chuck = require('chucknorris-io'),
-  client = new Chuck();
+const Chuck = require('chucknorris-io');
+const client = new Chuck();
 
 module.exports = (event, context) => {
+
   let err;
 
-
   client.getRandomJoke('dev').then(function (response) {
+
     const result2 = response.getValue();
-    console.log(result2);
+
     context
       .status(200)
       .succeed(result2);
-  }).catch(function (err) {
-    context.fail(err.message);
-  });
 
-  ///context
-  ///    .status(200)
-  ///    .succeed(result);
+  }).catch(function (err) {
+
+    context.fail(err.message);
+
+  });
 }
